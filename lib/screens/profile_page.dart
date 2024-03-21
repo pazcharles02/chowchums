9b354931e3365446874d99625166e5c111de2c2a
unknown <wueddyw@gmail.com>
2024-03-21 06:00:17 -0700

    profile picture added

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfilePage extends StatefulWidget {
   final String userId;
@@ -11,15 +15,35 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  String _profileImageUrl = ""; // Store profile image URL chowchums-2c5b3.appspot.com
+
   void _signOut() async {
     try {
       await FirebaseAuth.instance.signOut();
       // Navigate to login page after log out
       Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
-      // Using pushNamedAndRemoveUntil removes all routes from the stack
-      // push the login page so that user can't go back to profile
     } catch (e) {
       print("Error signing out: $e");
+    }
+  }
+
+  Future<void> _uploadProfilePicture(String filePath) async {
+    try {
+      final storage = FirebaseStorage.instance;
+      final reference = storage.ref().child('profile_pictures/${widget.userId}');
+      final uploadTask = reference.putFile(File(filePath));
+      await uploadTask.whenComplete(() => print('Image uploaded'));
+      final url = await reference.getDownloadURL();
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(widget.userId)
+          .update({'profileImageUrl': url});
+      setState(() {
+        _profileImageUrl = url;
+        print("SSSSSSSSSSSSSSSSSSSSS" + _profileImageUrl);
+      });
+    } catch (e) {
+      print("Error uploading profile picture: $e");
     }
   }
 
@@ -34,6 +58,8 @@ class _ProfilePageState extends State<ProfilePage> {
           return Text('Error fetching data');
         } else {
           final displayName = snapshot.data!.get('displayName');
+          _profileImageUrl = snapshot.data!.get('profileImageUrl');
+          print("AAAAAAAAAAAAAAAAAAAAAAAAA" + _profileImageUrl);
           return Scaffold(
             body: SingleChildScrollView(
               child: Column(
@@ -42,9 +68,29 @@ class _ProfilePageState extends State<ProfilePage> {
                   Padding(
                     padding: const EdgeInsets.all(20.0),
                     child: Center(
-                    child: Text(
-                      '$displayName!',
-                      style: TextStyle(fontSize: 24),
+                      child: Text(
+                        '$displayName!',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                  ),
+                  // Profile picture section
+                  GestureDetector(
+                    onTap: () async {
+                      // Handle image selection (use a library like image_picker)
+                      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+                      if (image != null) {
+                        await _uploadProfilePicture(image.path);
+                      }
+                    },
+                    child: Container(
+                      width: 100, // Fixed width
+                      height: 100, // Fixed height
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundImage: _profileImageUrl.isEmpty
+                            ? AssetImage('assets/images/default_picture.png') // Use AssetImage for local image
+                            : NetworkImage(_profileImageUrl) as ImageProvider,
                       ),
                     ),
                   ),
