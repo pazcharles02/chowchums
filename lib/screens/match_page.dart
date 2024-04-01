a4e1eaf494789b5cf780ffef47a9f87ac623d858
Khn167 <knguyen101@my.bcit.ca>
2024-03-31 18:03:14 -0700

    can get users in card for matchedpage.

--- lib/screens/match_page.dart
+++ lib/screens/match_page.dart
@@ -1,57 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
-
-List<Container> cards = [
-    Container(
-      alignment: Alignment.center,
-      child: const Text('1'),
-      color: Colors.blue,
-    ),
-    Container(
-      alignment: Alignment.center,
-      child: const Text('2'),
-      color: Colors.red,
-    ),
-    Container(
-      alignment: Alignment.center,
-      child: const Text('3'),
-      color: Colors.purple,
-    )
-  ];
 
 class MatchPage extends StatefulWidget {
   final String userId;
+
   const MatchPage({Key? key, required this.userId}) : super(key: key);
-  
 
   @override
   _MatchPageState createState() => _MatchPageState();
 }
 
 class _MatchPageState extends State<MatchPage> {
+  late Future<QuerySnapshot<Map<String, dynamic>>> _userFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _userFuture = FirebaseFirestore.instance.collection('users').get();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<DocumentSnapshot>(
-      future: FirebaseFirestore.instance.collection('users').doc(widget.userId).get(),
+    return FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+      future: _userFuture,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          //LOADING SPINNER wow
           return Center(child: CircularProgressIndicator());
         } else if (snapshot.hasError) {
-          // Show an error message if there's an error
           return Text('Error fetching data');
+        } else if (snapshot.data == null || snapshot.data!.docs.isEmpty) {
+          return Text('No user data found');
         } else {
-          //just to check if user is logged in take out later
-          final displayName = snapshot.data!.get('displayName');
+          final userData = snapshot.data!.docs;
+          List<Widget> fetchedData = userData.map((doc) {
+            final name = doc['displayName'];
+            final favoriteFood = doc['favoriteFood'];
+            return Container(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('User Display Name: $name'),
+                ],
+              ),
+              color: Colors.blue,
+            );
+          }).toList();
           return Scaffold(
-            body: Flexible(
-              child: CardSwiper(
-                cardsCount: 3,
-                cardBuilder: (context, index, percentThresholdX, percentThresholdY) => cards[index],
-              )
-            )
+            body: CardSwiper(
+              cardsCount: fetchedData.length,
+              cardBuilder: (context, index, percentThresholdX, percentThresholdY) =>
+                  fetchedData[index],
+            ),
           );
         }
       },
